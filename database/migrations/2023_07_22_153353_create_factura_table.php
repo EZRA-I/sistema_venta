0f50720e231b9ab31e583d5509faa3e49a435cb5
EZRA-I <firulai.martinez007@gmail.com>
2023-07-22 11:33:00 -0500

    1 commit

---
+++ database/migrations/2023_07_22_153353_create_factura_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factura', function (Blueprint $table) {
+            $table->id();
+            $table->text('descripcion');
+
+            $table->bigInteger('empleado_id')->unsigned();
+            $table->bigInteger('cliente_id')->unsigned();
+            $table->bigInteger('producto_id')->unsigned();
+
+            $table->foreign('empleado_id')->references('id')->on('empleados')->onDelete('cascade');
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('cascade');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factura');
+    }
+};
